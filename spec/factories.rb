bfa6eaf315b730b3ee2ab9a2da714040c4b0f224
hyon lee <hyon.lee@utexas.edu>
2019-08-28 20:09:36 +0000

    rspec debug fixed

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
   factory :gram do
     message { "hello" }
     #???
-    #picture {loads/gram/picture/2/robots.png}
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
     association :user
   end
 end
